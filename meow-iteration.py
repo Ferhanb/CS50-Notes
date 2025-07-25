ba3418fe08ac7991f880d6b70df9371168ed82af
Ferhanb <balci.ferhan@outlook.com>
2025-07-25 19:40:25 +0200

    Add files via upload

---
+++ meow-iteration.py
@@ -0,0 +1,29 @@
+# i = 0
+# while i < 3:
+    #print("meow")
+    #i += 1
+
+#for i in [0, 1, 2]:
+   # print("meow")
+
+#for i in ["cat", "bird", "dog"]:
+ #   print("meow")
+
+#for i in range(3)
+ #   print("meow")
+
+#for _ in range(3)
+  #  print("meow")
+
+#=====================
+
+def main():
+    meow(3)
+
+def meow(n):
+    for i in range(n):
+        print("meow")
+
+if __name__ == "__main__":
+main()
+#-----------------------
